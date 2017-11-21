63952498d83e36b5fc07d2fa13790ffecb126263
Paul Kiddle <neonpaul@gmail.com>
2017-11-21 20:45:34 +0000

    Initial code commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const hasKey = key => ([k]) => k === key;
+const notKey = key => ([k]) => k !== key;
+const hasPair = (key, val) => ([k, v]) => k === key && v == val;
+
+module.exports = class Query {
+  constructor(fields = []) {
+    if (Array.isArray(fields)) {
+      this.fields = fields;
+      return;
+    }
+
+    if (typeof fields === "string") {
+      return Query.fromString(fields);
+    }
+
+    if (typeof fields === "object") {
+      return Query.fromObject(fields);
+    }
+
+    throw new TypeError(
+      `Query expects array, string or object, not ${typeof fields}`
+    );
+  }
+
+  static fromString(string) {
+    const fields = string
+      .replace(/^\?/, "")
+      .split("&")
+      .map(seg => seg.split("=").map(decodeURIComponent));
+    return new Query(fields);
+  }
+
+  static fromObject(object) {
+    return new Query(Object.entries(object));
+  }
+
+  append(key, value) {
+    this.fields.push([key, value]);
+  }
+
+  get(key) {
+    return this.fields.find(hasKey(key))[1];
+  }
+
+  getAll(key) {
+    return this.fields.filter(hasKey(key)).map(f => f[0]);
+  }
+
+  delete(key) {
+    this.fields = this.fields.filter(notKey(key));
+  }
+
+  entries() {
+    return this.fields.entries();
+  }
+
+  has(key, value) {
+    return this.fields.some(hasPair(key, value));
+  }
+
+  set(key, [...values]) {
+    this.delete(key);
+    this.fields.concat(values.map(v => [key, v]));
+  }
+
+  [Symbol.iterator]() {
+    return this.fields[Symbol.iterator]();
+  }
+
+  toString() {
+    return (
+      "?" +
+      this.fields
+        .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+        .join("&")
+    );
+  }
+};
---
+++ index.test.js
@@ -0,0 +1,12 @@
+const Query = require(".");
+
+test("It parses query strings", () => {
+  const q = new Query("?a=b");
+  expect(q.get("a")).toEqual("b");
+});
+
+test("It builds query strings", () => {
+  const q = new Query();
+  q.append("c", "d");
+  expect(q.toString()).toEqual("?c=d");
+});
